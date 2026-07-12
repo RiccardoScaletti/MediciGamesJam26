45dec690f857db1bb4a5dc7251560aa456e8bd6e
Franco Ramos <f.ramos57.09.5@gmail.com>
2026-07-12 15:59:48 -0500

    Temp Animations
    Created animation controller,
    Animation Clips:
    forward, backwards, idle, forward transition, backwards transition
    Adjusted transition timing

---
+++ Assets/_Scripts/Animation/PlayerAnimation.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    //take input -1 to 1
+    //decide to apply backwards if less than zero, apply forwards at greater than zero
+
+    //stretch goals: scale speed to input value
+
+    private Animator animator;
+
+    [SerializeField] private float testFloat;
+
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        if(animator == null) { Debug.Log("<color=red>animator component not found!</color>"); }
+    }
+
+    private void Update()
+    {
+        animator.SetFloat("Direction", testFloat);
+    }
+}
