b3204441d0677e584eb5bc625cf8b714ccfa3f6c
Riccardo Scaletti <rickgamedeveloper@gmail.com>
2026-07-12 18:32:25 -0500

    First arena
    First rectangular arena made, rotating on itself it still provides ways to get to the end

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance { get; private set; }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+    }
+    
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        
+    }
+}
